bc2263f59f352fab13a5e59231ad1c3b289e1501
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-02-11 03:05:45 +0000

    added illustration

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import IntroIllustration from "./misc/Background";
 
 export default function Intro() {
   return (
-    <div className="no-header-height w-full">
-      <div className="rounded-lg mt-16">
+    <div className="w-full flex flex-col md:flex-row ">
+      <div className="rounded-lg flex-1 flex flex-col items-center justify-center">
         <h1 className="text-8xl font-bold">Nocxa</h1>
         <p className="text-xl font-light">
           Nocturnal <span className="text-[var(--purple)]">x</span> Agency
         </p>
+      </div>
+
+      <div className="flex-1 flex items-center justify-center">
+        <IntroIllustration className="w-4/6" />
       </div>
     </div>
   );
