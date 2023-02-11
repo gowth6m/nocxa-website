e2c8e283e853d8fddc67ee385e7e8d89d895faef
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-02-11 01:17:55 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const pathPrefix = process.env.NODE_ENV === 'production'
-  ? 'https://gowth6mxxx.github.io/nocxa-website/'
-  : '';
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost']
-  },
-  assetPrefix: pathPrefix,
-  env: {
-    pathPrefix,
   },
 }
 
