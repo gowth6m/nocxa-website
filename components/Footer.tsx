4db984d0f0c23ccc67670e37f0af07bcdbc339f6
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-02-12 14:28:20 +0000

    trying fix for iphone

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ import { NocxaLogo } from "./misc/Icons";
 export default function Footer() {
   return (
     <div className="my-8 mb-10">
-      <div className="container w-full">
+      <div className="container w-full mx-2">
         <div className="flex flex-col md:flex-row md:gap-4 gap-12">
           <div className="flex flex-1 flex-row flex-wrap gap-4 md:order-2 justify-center items-center">
             <Link className="hover:text-[var(--purple)]" href="#about">
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -50,17 +50,17 @@ export default function NavBar() {
           className={
             visible
               ? "fixed top-0 w-full flex justify-center items-center h-16"
-              : "fixed top-0 w-full flex justify-center items-center h-16 bg-[var(--navblack)]"
+              : "fixed top-0 w-full flex justify-center items-center h-16 md:bg-[var(--navblack)] bg-transparent"
           }
         >
           <div className="container fixed mx-auto h-16 flex font-museo">
             {/* MOBILE */}
             <div className="flex justify-between items-center w-full mx-1 md:mx-4">
               {/* LOGO */}
-              <div className="h-8 md:flex-1 flex justify-start md:w-full">
+              <div className="h-8 md:flex-1 flex justify-start md:w-full mx-0">
                 <NocxaLogo
                   secondaryColor="var(--purple)"
-                  className="md:mr-auto md:w-auto"
+                  className="md:mr-auto md:w-auto mx-0"
                 />
               </div>
 
