b5933c2b003b07beff9328d7bd5f25cbbc02d279
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-02-14 01:25:13 +0000

    updated about page

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,22 +7,16 @@ import { ArrowArt, Illustration1 } from "./misc/Illustrations.";
 export default function Intro() {
   return (
     <>
-      {/* <div>
-        <div className="hidden md:flex bg-[var(--orange)] absolute w-4 h-[3rem] right-5 rounded-full bottom-[8rem]"></div>
-        <div className="hidden md:flex bg-[var(--orange)] absolute w-4 h-[2rem] right-5 rounded-full bottom-[5rem]"></div>
-        <div className="hidden md:flex bg-[var(--orange)] absolute w-4 h-4 right-5 rounded-full bottom-[3rem]"></div>
-      </div> */}
-
       {/* Desktop */}
       <div className="container mx-auto h-auto hidden md:flex">
-        <div className="w-full flex flex-col md:flex-row relative px-4 mb-20">
+        <div className="w-full flex flex-col md:flex-row relative px-4 mb-12">
           <div className="flex-1 flex flex-col">
             <h1 className="text-4xl md:text-8xl font-bold grad-bg-text font-museo mt-8 md:mt-24 lg:mt-40">
               Bringing new ideas to life.
             </h1>
             <div className="flex flex-row">
               <div className="mt-8 md:my-8 w-auto text-lg md:text-xl font-museo px-4 py-1 md:py-2 bg-[var(--orange)] border-2 border-[var(--orange)] text-[var(--lightWhite)] rounded-full cursor-pointer hover:bg-[var(--pink)] hover:border-[var(--pink)]">
-                View our work
+                See our work
               </div>
             </div>
           </div>
@@ -44,8 +38,6 @@ export default function Intro() {
           </div>
           <div className="flex flex-row w-[80%]">
             <div className="text-3xl font-bold font-museo w-4/6 text-right ml-auto my-2">
-              {/* <span className="grad-bg-text">Noc</span>turnal{" "}
-              <span className="grad-bg-text">x A</span>gency */}
               See our <br />
               <span className="text-[var(--purple)]"> work</span>
             </div>
--- components/Services.tsx
+++ components/Services.tsx
@@ -1,29 +1,57 @@
 import React from "react";
+import Illustration2 from "./misc/Illustrations.";
 
 export default function Services() {
   return (
-    <div className="min-h-[100svh] w-[100vw]">
-      <div className="container mx-auto px-4 md:px-2">
-        <div className="rounded-3xl bg-[var(--leafgrey)] min-h-[100svh]">
-          <div className="flex flex-col justify-between items-center w-full px-4 md:px-8">
-            <div className="flex flex-col font-museo my-8 w-full text-left justify-center align-middle">
-              <div className="italic grad-bg-text text-lg md:text-2xl">
-                Services
+    <div className="h-auto w-[100vw]">
+      <div className="container mx-auto p-4 md:p-0 h-full my-2 md:my-8 md:w-[100%]">
+        <div className="rounded-3xl bg-[var(--leafgrey)] h-full">
+          <div className="flex flex-col justify-between items-center w-full px-4 md:px-8 h-full md:flex-row">
+            <div className="flex flex-col md:flex-1">
+              <div className="flex flex-col font-museo my-8 w-full text-left justify-center align-middle">
+                <div className="italic grad-bg-text text-lg md:text-2xl">
+                  ABOUT US
+                </div>
+                <h1 className="text-3xl md:text-5xl font-bold text-[var(--cream)] w-full text-left">
+                  What&#39;s Nocxa?
+                </h1>
               </div>
-              <h1 className="text-3xl md:text-5xl font-bold text-[var(--cream)] w-full text-left">
-                What can we do?
-              </h1>
+              <div className="text-[var(--cream)] flex font-comfortaa text-sm md:mb-8">
+                <div className="md:text-base">
+                  Nocxa is a{" "}
+                  <span className="grad-bg-text text-lg italic">
+                    {" "}
+                    student-led{" "}
+                  </span>{" "}
+                  software company is dedicated to delivering innovative web
+                  development, app development and machine learning solutions.
+                  <br />
+                  <br />
+                  Our team of talented and motivated developers provide
+                  cutting-edge technology services to businesses and
+                  individuals.
+                  <div className="hidden md:block">
+                    <br />
+                    With a focus on{" "}
+                    <span className="grad-bg-text text-lg italic">
+                      {" "}
+                      creativity{" "}
+                    </span>{" "}
+                    and a passion for problem-solving, we aim to deliver dynamic
+                    and intuitive software that meets the unique needs of each
+                    of our clients. From website design to app development and
+                    beyond, our goal is to help our clients bring their{" "}
+                    <span className="grad-bg-text text-lg italic">
+                      {" "}
+                      visions to life{" "}
+                    </span>{" "}
+                    and stay ahead in today&#39;s fast-paced digital world.
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="text-[var(--cream)] flex font-comfortaa">
-              <div className="md:w-3/6">
-                Nocxa is a student-led software company is dedicated to
-                delivering innovative web development, app development and
-                machine learning solutions.
-                <br />
-                <br />
-                Our team of talented and motivated developers provide
-                cutting-edge technology services to businesses and individuals.
-              </div>
+            <div className="flex w-full items-center justify-center mt-4 mb-8 md:flex-1">
+              <Illustration2 className="h-40 w-5/6 md:w-full md:h-80" />
             </div>
           </div>
         </div>
