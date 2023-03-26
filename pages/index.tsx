2b1da1b6ced0ca245f9c4b5599c6b73ea57d3d5d
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-03-26 04:51:58 +0100

    Fix for invalid css

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import Layout from "@/components/Layout";
 import Intro from "@/components/Intro";
 import About from "@/components/About";
 import Services from "@/components/Services";
+import "@/styles/globals.css";
+import "@/styles/animations.css";
+import "@/styles/misc.css";
 
 export default function Home() {
   return (
